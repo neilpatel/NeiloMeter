d1c48014eb7d9499668a65f5949c49bad6efe4be
Neil Patel <nmp61@pitt.edu>
2018-07-30 19:36:01 -0400

    AppUpdated Receiver

---
+++ app/src/src/main/java/AppUpdatedReceiver.java
@@ -0,0 +1,21 @@
+/*
+Neil Patel
+GitHub: neilpatel
+Personal Project
+*/
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AppUpdatedReceiver extends BroadcastReceiver {
+
+	
+	public void onReceive(final Context context, final Intent intent) {
+		if (BuildConfig.DEBUG)
+			Logger.log("app updated");
+		context.startService(new Intent(context, SensorListener.class));		
+	}
+
+}
