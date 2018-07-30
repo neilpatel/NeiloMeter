8c7ced2476c4e907cc50d6f3b08a8edab4e23480
Neil Patel <nmp61@pitt.edu>
2018-07-30 19:40:49 -0400

    Add Database file

---
+++ app/src/src/main/java/Database.java
@@ -0,0 +1,67 @@
+/*
+Neil Patel
+GitHub: neilpatel
+Personal Project
+*/
+
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import util.Logger;
+import de.j4velin.pedometer.util.Util;
+
+public class Database extends SQLiteOpenHelper {
+
+    private final static String DB_NAME = "steps";
+    private final static int DB_VERSION = 2;
+
+    private static Database instance;
+    private static final AtomicInteger openCounter = new AtomicInteger();
+
+    private Database(final Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    public static synchronized Database getInstance(final Context c) {
+        if (instance == null) {
+            instance = new Database(c.getApplicationContext());
+        }
+        openCounter.incrementAndGet();
+        return instance;
+    }
+
+    @Override
+    public void close() {
+        if (openCounter.decrementAndGet() == 0) {
+            super.close();
+        }
+    }
+
+    @Override
+    public void onCreate(final SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + DB_NAME + " (date INTEGER, steps INTEGER)");
+    }
+
+    @Override
+    public void onUpgrade(final SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion == 1) {
+            // drop PRIMARY KEY constraint
+            db.execSQL("CREATE TABLE " + DB_NAME + "2 (date INTEGER, steps INTEGER)");
+            db.execSQL("INSERT INTO " + DB_NAME + "2 (date, steps) SELECT date, steps FROM " +
+                    DB_NAME);
+            db.execSQL("DROP TABLE " + DB_NAME);
+            db.execSQL("ALTER TABLE " + DB_NAME + "2 RENAME TO " + DB_NAME + "");
+        }
+    }
+
+   
